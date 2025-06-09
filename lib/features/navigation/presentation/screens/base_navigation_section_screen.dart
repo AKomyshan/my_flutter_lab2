a3e195e0b322c21a0bc1a77b2077e04b27b75542
Pavlo Avdonin <p.avdonin@foodtech.team>
2025-06-09 12:52:56 +0300

    feat(LAB-16): Implemented navigation with returning data and added a new screen for data presentation.

--- lib/features/navigation/presentation/screens/base_navigation_section_screen.dart
+++ lib/features/navigation/presentation/screens/base_navigation_section_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lab/features/navigation/presentation/screens/simple_empty_screen.dart';
 import 'package:flutter_lab/features/navigation/presentation/screens/simple_screen_with_data.dart';
+import 'package:flutter_lab/features/navigation/presentation/screens/simple_screen_with_returning_data.dart';
 import 'package:flutter_lab/features/navigation/presentation/widgets/navigation_button.dart';
 
 class BaseNavigationSectionScreen extends StatelessWidget {
@@ -37,8 +38,53 @@ class BaseNavigationSectionScreen extends StatelessWidget {
               );
             },
           ),
+          NavagationButton(
+            title: 'Transition with returning data',
+            onTap: () async {
+              final result = await Navigator.push(
+                context,
+                MaterialPageRoute<String>(
+                  builder: (context) =>
+                      const SimpleScreenWithReturningData(guestName: 'Pavlo'),
+                ),
+              );
+
+              if (result != null && context.mounted) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  _createSnackBar(context, result),
+                );
+              }
+            },
+          ),
         ],
       ),
     );
   }
+}
+
+SnackBar _createSnackBar(BuildContext context, String result) {
+  return SnackBar(
+    content: Row(
+      children: [
+        const Icon(Icons.card_giftcard, color: Colors.white),
+        const SizedBox(width: 12),
+        Expanded(
+          child: Text(
+            result,
+            style: const TextStyle(fontSize: 16),
+          ),
+        ),
+      ],
+    ),
+    behavior: SnackBarBehavior.floating,
+    duration: const Duration(seconds: 4),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(6)),
+    ),
+    padding: const EdgeInsets.symmetric(
+      horizontal: 16,
+      vertical: 12,
+    ),
+    dismissDirection: DismissDirection.none,
+  );
 }
