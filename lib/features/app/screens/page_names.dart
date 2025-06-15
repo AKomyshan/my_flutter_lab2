b56ddf4efbd2cc1856432b0a9240bded7c22ce46
Pavlo Avdonin <p.avdonin@foodtech.team>
2025-06-15 15:54:02 +0300

    feat(LAB-17):  Switching to go_router

--- lib/features/app/screens/page_names.dart
+++ lib/features/app/screens/page_names.dart
@@ -2,4 +2,9 @@ abstract class ScreenNames {
   static const home = 'home';
   static const widgets = 'widgets';
   static const navigation = 'navigation';
+  static const widgetsFirstPart = 'widgets-first-part';
+  static const widgetsSecondPart = 'widgets-second-part';
+  static const understandingConstraints = 'understanding-constraints';
+  static const homework13 = 'homework-13';
+  static const homework14 = 'homework-14';
 }
--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_lab/features/app/screens/home_screen.dart';
 import 'package:flutter_lab/features/app/screens/page_names.dart';
+import 'package:flutter_lab/features/homeworks/lesson_13/homework_13_screen.dart';
+import 'package:flutter_lab/features/homeworks/lesson_14/homework_14_screen.dart';
 import 'package:flutter_lab/features/navigation/presentation/screens/navigation_main_screen.dart';
+import 'package:flutter_lab/features/widgets/presentation/screens/widgets_first_part_screen.dart';
 import 'package:flutter_lab/features/widgets/presentation/screens/widgets_main_screen.dart';
+import 'package:flutter_lab/features/widgets/presentation/screens/widgets_second_part_screen.dart';
+import 'package:flutter_lab/features/widgets/presentation/screens/widgets_third_part_screen.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
@@ -14,6 +19,33 @@ final router = GoRouter(
           path: 'widgets',
           name: ScreenNames.widgets,
           builder: (context, state) => const WidgetsScreen(),
+          routes: [
+            GoRoute(
+              path: 'widgets-first-part',
+              name: ScreenNames.widgetsFirstPart,
+              builder: (context, state) => const WidgetsFirstPartScreen(),
+            ),
+            GoRoute(
+              path: 'widgets-second-part',
+              name: ScreenNames.widgetsSecondPart,
+              builder: (context, state) => const WidgetsSecondPartScreen(),
+            ),
+            GoRoute(
+              path: 'understanding-constraints',
+              name: ScreenNames.understandingConstraints,
+              builder: (context, state) => const WidgetsThirdPartScreen(),
+            ),
+            GoRoute(
+              path: 'homework-13',
+              name: ScreenNames.homework13,
+              builder: (context, state) => const Homework13Screen(),
+            ),
+            GoRoute(
+              path: 'homework-14',
+              name: ScreenNames.homework14,
+              builder: (context, state) => const Homework14Screen(),
+            ),
+          ],
         ),
         GoRoute(
           path: 'navigation',
