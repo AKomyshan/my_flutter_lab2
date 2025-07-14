dc55db7fdb9bff47a9e90534a8efd526f2fd670c
onlytarg-clone <avdoninpav@gmail.com>
2025-07-14 18:03:59 +0300

    feat(LAB-22): Added animation controller example.

--- lib/features/animations/presentation/explicit_animations/examples/animation_controller.dart
+++ lib/features/animations/presentation/explicit_animations/examples/animation_controller.dart
@@ -1,7 +1,48 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
-class AnimationControllerExample extends StatelessWidget {
+// Показати зміну значення _controller.value в консолі
+// Показати зміну значення _counter в консолі, чому саме така кількість
+// TickerProviderStateMixin та SingleTickerProviderStateMixin
+// setState()
+
+class AnimationControllerExample extends StatefulWidget {
   const AnimationControllerExample({super.key});
+
+  @override
+  State<AnimationControllerExample> createState() =>
+      _AnimationControllerExampleState();
+}
+
+class _AnimationControllerExampleState extends State<AnimationControllerExample>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    _controller
+      ..addListener(() {
+        print(_counter++);
+        print(_controller.value);
+      })
+      ..forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +50,8 @@ class AnimationControllerExample extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Animation Controller'),
       ),
-      body: const Center(
-        child: Text('Animation Controller'),
+      body: Center(
+        child: Text(_controller.value.toStringAsFixed(2)),
       ),
     );
   }
