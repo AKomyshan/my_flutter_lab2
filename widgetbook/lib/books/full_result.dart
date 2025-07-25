8eda060f422142a085d0d934cc085f7c4d945ddc
antonkomyshan <anton.komyshan@namecheap.com>
2025-07-25 14:42:07 +0300

    chore: add full result preview

---
+++ widgetbook/lib/books/full_result.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_lab/features/homeworks/lesson_14/homework_14_screen.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+@widgetbook.UseCase(name: 'Default', type: Homework14Screen)
+Widget buildRatingUseCase(BuildContext context) {
+  return Homework14Screen();
+}
--- widgetbook/lib/main.directories.g.dart
+++ widgetbook/lib/main.directories.g.dart
@@ -11,8 +11,9 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:widgetbook/widgetbook.dart' as _i1;
-import 'package:widgetbook_workspace/books/custom_card_book.dart' as _i2;
-import 'package:widgetbook_workspace/books/rating_book.dart' as _i3;
+import 'package:widgetbook_workspace/books/custom_card_book.dart' as _i3;
+import 'package:widgetbook_workspace/books/full_result.dart' as _i2;
+import 'package:widgetbook_workspace/books/rating_book.dart' as _i4;
 
 final directories = <_i1.WidgetbookNode>[
   _i1.WidgetbookFolder(
@@ -24,6 +25,13 @@ final directories = <_i1.WidgetbookNode>[
           _i1.WidgetbookFolder(
             name: 'lesson_14',
             children: [
+              _i1.WidgetbookLeafComponent(
+                name: 'Homework14Screen',
+                useCase: _i1.WidgetbookUseCase(
+                  name: 'Default',
+                  builder: _i2.buildRatingUseCase,
+                ),
+              ),
               _i1.WidgetbookFolder(
                 name: 'components',
                 children: [
@@ -31,14 +39,14 @@ final directories = <_i1.WidgetbookNode>[
                     name: 'CustomCard',
                     useCase: _i1.WidgetbookUseCase(
                       name: 'Default',
-                      builder: _i2.buildCustomCardUseCase,
+                      builder: _i3.buildCustomCardUseCase,
                     ),
                   ),
                   _i1.WidgetbookLeafComponent(
                     name: 'Rating',
                     useCase: _i1.WidgetbookUseCase(
                       name: 'Default',
-                      builder: _i3.buildRatingUseCase,
+                      builder: _i4.buildRatingUseCase,
                     ),
                   ),
                 ],
