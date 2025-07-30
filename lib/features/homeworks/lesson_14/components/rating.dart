105d42b1d0710a972c184a8a0fad5ed548eaad11
antonkomyshan <anton.komyshan@namecheap.com>
2025-07-30 20:59:03 +0300

    fix(LAB-17): homework

--- lib/features/homeworks/lesson_14/components/rating.dart
+++ lib/features/homeworks/lesson_14/components/rating.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_lab/extensions/build_context_extension.dart';
 import 'package:flutter_lab/features/homeworks/lesson_14/typedefs.d.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -18,44 +20,45 @@ class _RatingState extends State<Rating> {
   int _currentRating = 0;
 
   void _handleStarTap(int tappedRating) {
-    setState(() {
-      _currentRating = tappedRating;
-    });
+    setState(() => _currentRating = tappedRating);
     widget.onChange?.call(tappedRating);
   }
 
   @override
   Widget build(BuildContext context) {
-    final isSmScreen = MediaQuery.of(context).size.width <= 360;
+    if (kDebugMode) print('Screen width: ${MediaQuery.of(context).size.width}');
     return Row(
-      spacing: isSmScreen ? 0 : 8,
+      spacing: context.isSmScreen ? 0 : 8,
       mainAxisAlignment: MainAxisAlignment.center,
       children: List.generate(
         5,
         (i) {
           final starNumber = i + 1;
           final isFilled = starNumber <= _currentRating;
-          return GestureDetector(
-            onTap: () => _handleStarTap(starNumber),
-            child: Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                boxShadow: isFilled
-                    ? <BoxShadow>[
-                        BoxShadow(
-                          blurRadius: 14.4,
-                          color: _glowColor,
-                          spreadRadius: -16,
-                        ),
-                      ]
-                    : null,
-              ),
-              child: SvgPicture.asset(
-                'assets/images/star.svg',
-                fit: BoxFit.contain,
-                colorFilter: isFilled
-                    ? null
-                    : ColorFilter.mode(Colors.grey.shade400, BlendMode.srcIn),
+          return Flexible(
+            flex: 1,
+            child: GestureDetector(
+              onTap: () => _handleStarTap(starNumber),
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  boxShadow: isFilled
+                      ? <BoxShadow>[
+                          BoxShadow(
+                            blurRadius: 14.4,
+                            color: _glowColor,
+                            spreadRadius: -16,
+                          ),
+                        ]
+                      : null,
+                ),
+                child: SvgPicture.asset(
+                  'assets/images/star.svg',
+                  fit: BoxFit.contain,
+                  colorFilter: isFilled
+                      ? null
+                      : ColorFilter.mode(Colors.grey.shade400, BlendMode.srcIn),
+                ),
               ),
             ),
           );
