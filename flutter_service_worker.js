ff473a96b2a441767c8c5955a8934fd45a181d6e
AKomyshan <AKomyshan@users.noreply.github.com>
2025-07-30 11:57:19 +0000

    deploy: e84a57f401e917e0a7f88499676a06b4b1e0372d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,7 +46,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b9e9b37332cea63dfd5cc86364a4ee0
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "825e75415ebd366b740bb49659d7a5c6",
 "version.json": "8b0f0570d207309cf1a8f4b5bfb7a8a0",
 "manifest.json": "9ff2d08cde31b5913f2900dc7795296f",
-"flutter_bootstrap.js": "d4f08deb61143abf0166e2880ff2273e",
+"flutter_bootstrap.js": "fd01f0285399e31e44a1ff6dd016e493",
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "canvaskit/canvaskit.wasm": "a37f2b0af4995714de856e21e882325c",
 "canvaskit/skwasm.js.symbols": "9fe690d47b904d72c7d020bd303adf16",
